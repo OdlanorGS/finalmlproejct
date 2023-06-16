41e93356133940932fb35faf4533650eb3514de1
Ronaldo Garcia Sevilla <112920921+OdlanorGS@users.noreply.github.com>
2023-06-16 18:56:23 -0400

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,3 +1,4 @@
+import streamlit as st
 st.title('My WebApp')  # Sets the title of your page
 st.header('Data Visualization Section')  # Sets a header for a section
 st.subheader('Subsection: Analysis')  # Sets a subheader for a subsection
