e757efe75f4a84f4f7b5e5023b7b2eccd4007948
Ronaldo Garcia Sevilla <112920921+OdlanorGS@users.noreply.github.com>
2023-06-16 18:55:46 -0400

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,1 +1,3 @@
- 
+st.title('My WebApp')  # Sets the title of your page
+st.header('Data Visualization Section')  # Sets a header for a section
+st.subheader('Subsection: Analysis')  # Sets a subheader for a subsection
